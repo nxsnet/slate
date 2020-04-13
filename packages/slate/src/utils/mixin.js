d32f5e23562ef451ced0753cc2eec8a5214d0445
AJ Palkovic <aj@discordapp.com>
2020-04-13 10:40:54 -0700

    Fix a crash in mobile safari

--- packages/slate/src/utils/mixin.js
+++ packages/slate/src/utils/mixin.js
@@ -11,14 +11,26 @@ export default function mixin(Interface, Classes) {
     for (const name of Object.getOwnPropertyNames(Interface)) {
       if (Class.hasOwnProperty(name)) continue
       const desc = Object.getOwnPropertyDescriptor(Interface, name)
-      Object.defineProperty(Class, name, desc)
+
+      if (desc == null) {
+        console.log(`Property descriptor was undefined for: ${name}`)
+        Class[name] = Class[name]
+      } else {
+        Object.defineProperty(Class, name, desc)
+      }
     }
 
     // Copy instance properties from the interface.
     for (const name of Object.getOwnPropertyNames(Interface.prototype)) {
       if (Class.prototype.hasOwnProperty(name)) continue
       const desc = Object.getOwnPropertyDescriptor(Interface.prototype, name)
-      Object.defineProperty(Class.prototype, name, desc)
+
+      if (desc == null) {
+        console.log(`Property prototype descriptor was undefined for: ${name}`)
+        Class.prototype[name] = Interface.prototype[name]
+      } else {
+        Object.defineProperty(Class.prototype, name, desc)
+      }
     }
   }
 }
